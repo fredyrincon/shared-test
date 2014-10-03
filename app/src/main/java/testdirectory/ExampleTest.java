a97d4caa2cafe501a8166888a23e2781a05b8b66
Fredy Rincon <fredyrincon@hotmail.com>
2014-10-03 16:15:58 +1000

    Second activity added

---
+++ app/src/main/java/testdirectory/ExampleTest.java
@@ -0,0 +1,12 @@
+package testdirectory;
+
+/**
+ * Created by SafeCode on 3/10/2014.
+ */
+public class ExampleTest extends  InstrumentationTestCase{
+    public void test() throws Exception {
+        final int expected = 1;
+        final int reality = 5;
+        assertEquals(expected, reality);
+    }
+}
